098a97841a814a6674c45929f2c4a62d2425298d
Nishu <nishanthravichandran12@gmail.com>
2024-09-01 19:49:23 +0530

    Use test.csv dynmically in csv viewer

--- index.js
+++ index.js
@@ -1,143 +1,146 @@
-// let charts = [];
-// let intervalId;
-// let a = 0;
+let charts = [];
+let intervalId;
+let a = 0;
+localStorage.setItem('powerStatus', 'off');
+document.querySelector('.power').addEventListener('click', function() {
+    localStorage.setItem('powerStatus', 'on');
+});
+document.querySelector('.power').addEventListener('click', function() {
+    this.classList.toggle('clicked');
+    if (this.classList.contains('clicked')) {
+        // Recreate charts if they don't exist
+        if (charts.length === 0) {
+            const ctx1 = document.getElementById('graph1').getContext('2d');
+            const ctx2 = document.getElementById('graph2').getContext('2d');
+            const ctx3 = document.getElementById('graph3').getContext('2d');
+            const ctx4 = document.getElementById('graph4').getContext('2d');
+            const ctx5 = document.getElementById('graph5').getContext('2d');
+            const ctx6 = document.getElementById('graph6').getContext('2d');
+            charts.push(createChart(ctx1, 'graph 1'));
+            charts.push(createChart(ctx2, 'graph 2'));
+            charts.push(createChart(ctx3, 'graph 3'));
+            charts.push(createChart(ctx4, 'graph 4'));
+            charts.push(createChart(ctx5, 'graph 5'));
+            charts.push(createChart(ctx6, 'graph 6'));
+        }
+        // Start updating charts
+        intervalId = setInterval(() => {
+            a++;
+            updateChart(charts[0], { x: a, y: Math.random() * 100 });
+            updateChart(charts[1], { x: a, y: Math.random() * 100 });
+            updateChart(charts[2], { x: a, y: Math.random() * 100 });
+            updateChart(charts[3], { x: a, y: Math.random() * 100 });
+            updateChart(charts[4], { x: a, y: Math.random() * 100 });
+            updateChart(charts[5], { x: a, y: Math.random() * 100 });
+        }, 1000);
+    } else {
+        // Stop updating charts
+        clearInterval(intervalId);
+        // Clear data inside charts
+        charts.forEach(chart => {
+            chart.data.labels = [];
+            chart.data.datasets.forEach(dataset => {
+                dataset.data = [];
+            });
+            chart.update();
+        });
+    }
+});
 
-// document.querySelector('.power').addEventListener('click', function() {
-//     this.classList.toggle('clicked');
-//     if (this.classList.contains('clicked')) {
-//         // Recreate charts if they don't exist
-//         if (charts.length === 0) {
-//             const ctx1 = document.getElementById('graph1').getContext('2d');
-//             const ctx2 = document.getElementById('graph2').getContext('2d');
-//             const ctx3 = document.getElementById('graph3').getContext('2d');
-//             const ctx4 = document.getElementById('graph4').getContext('2d');
-//             const ctx5 = document.getElementById('graph5').getContext('2d');
-//             const ctx6 = document.getElementById('graph6').getContext('2d');
-//             charts.push(createChart(ctx1, 'graph 1'));
-//             charts.push(createChart(ctx2, 'graph 2'));
-//             charts.push(createChart(ctx3, 'graph 3'));
-//             charts.push(createChart(ctx4, 'graph 4'));
-//             charts.push(createChart(ctx5, 'graph 5'));
-//             charts.push(createChart(ctx6, 'graph 6'));
-//         }
-//         // Start updating charts
-//         intervalId = setInterval(() => {
-//             a++;
-//             updateChart(charts[0], { x: a, y: Math.random() * 100 });
-//             updateChart(charts[1], { x: a, y: Math.random() * 100 });
-//             updateChart(charts[2], { x: a, y: Math.random() * 100 });
-//             updateChart(charts[3], { x: a, y: Math.random() * 100 });
-//             updateChart(charts[4], { x: a, y: Math.random() * 100 });
-//             updateChart(charts[5], { x: a, y: Math.random() * 100 });
-//         }, 1000);
-//     } else {
-//         // Stop updating charts
-//         clearInterval(intervalId);
-//         // Clear data inside charts
-//         charts.forEach(chart => {
-//             chart.data.labels = [];
-//             chart.data.datasets.forEach(dataset => {
-//                 dataset.data = [];
-//             });
-//             chart.update();
-//         });
-//     }
-// });
+function createChart(ctx, label) {
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: label,
+                data: [],
+                borderWidth: 1,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                }
+            }
+        }
+    });
+}
 
-// function createChart(ctx, label) {
-//     return new Chart(ctx, {
-//         type: 'line',
-//         data: {
-//             labels: [],
-//             datasets: [{
-//                 label: label,
-//                 data: [],
-//                 borderWidth: 1,
-//                 fill: false
-//             }]
-//         },
-//         options: {
-//             scales: {
-//                 x: {
-//                     type: 'linear',
-//                     position: 'bottom'
-//                 }
-//             }
-//         }
-//     });
-// }
+function updateChart(chart, newData) {
+    chart.data.labels.push(newData.x);
+    chart.data.datasets[0].data.push(newData.y);
+    chart.update();
+}
 
-// function updateChart(chart, newData) {
-//     chart.data.labels.push(newData.x);
-//     chart.data.datasets[0].data.push(newData.y);
-//     chart.update();
-// }
+document.querySelector('.flight').addEventListener('click', function() {
+    this.classList.toggle('clicked');
+});
 
-// document.querySelector('.flight').addEventListener('click', function() {
-//     this.classList.toggle('clicked');
-// });
-
-// document.querySelector('.sim').addEventListener('click', function() {
-//     this.classList.toggle('clicked');
-// });
+document.querySelector('.sim').addEventListener('click', function() {
+    this.classList.toggle('clicked');
+});
 
 
-// // Initialize the data arrays
-// var x = [];
-// var y = [];
-// var z = [];
+// Initialize the data arrays
+var x = [];
+var y = [];
+var z = [];
 
-// // Initial plot data and layout
-// var data = [{
-//     x: x,
-//     y: y,
-//     z: z,
-//     mode: 'lines',
-//     type: 'scatter3d'
-// }];
+// Initial plot data and layout
+var data = [{
+    x: x,
+    y: y,
+    z: z,
+    mode: 'lines',
+    type: 'scatter3d'
+}];
 
-// var layout = {
-//     title: '3D Line Plot',
-//     autosize: true,
-//     scene: {
-//         xaxis: { title: 'X Axis' },
-//         yaxis: { title: 'Y Axis' },
-//         zaxis: { title: 'Z Axis' }
-//     }
-// };
+var layout = {
+    title: '3D Line Plot',
+    autosize: true,
+    scene: {
+        xaxis: { title: 'X Axis' },
+        yaxis: { title: 'Y Axis' },
+        zaxis: { title: 'Z Axis' }
+    }
+};
 
-// // Create the initial plot
-// Plotly.newPlot('graph-image', data, layout);
+// Create the initial plot
+Plotly.newPlot('graph-image', data, layout);
 
-// // Function to update the data
-// function updateData() {
-//     // Generate new random data
-//     var newX = Math.random() * 10;
-//     var newY = Math.random() * 10;
-//     var newZ = Math.random() * 10;
+// Function to update the data
+function updateData() {
+    // Generate new random data
+    var newX = Math.random() * 10;
+    var newY = Math.random() * 10;
+    var newZ = Math.random() * 10;
 
-//     // Append new data to the arrays
-//     x.push(newX);
-//     y.push(newY);
-//     z.push(newZ);
+    // Append new data to the arrays
+    x.push(newX);
+    y.push(newY);
+    z.push(newZ);
 
-//     // Keep the array length fixed
-//     if (x.length > 50) {
-//         x.shift();
-//         y.shift();
-//         z.shift();
-//     }
+    // Keep the array length fixed
+    if (x.length > 50) {
+        x.shift();
+        y.shift();
+        z.shift();
+    }
 
-//     // Update the plot
-//     Plotly.update('graph-image', {
-//         x: [x],
-//         y: [y],
-//         z: [z]
-//     }, [0]); 
-// }
+    // Update the plot
+    Plotly.update('graph-image', {
+        x: [x],
+        y: [y],
+        z: [z]
+    }, [0]); 
+}
 
-// // Update the graph every second
-// setInterval(updateData, 1000);
+// Update the graph every second
+setInterval(updateData, 1000);
 
 
 
