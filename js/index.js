d2124728bbe026f1d52e9844b102f125aa211ca5
Nishu <nishanthravichandran12@gmail.com>
2024-09-06 11:12:28 +0530

    Power command should turn on everything

--- js/index.js
+++ js/index.js
@@ -351,3 +351,37 @@ function display_fss() {
 //     }
 //   });
 
+document.getElementById('commandInput').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        var inputText = this.value.toLowerCase();
+        var outputBox = document.getElementById('outputBox');
+
+        // Handle 'power on' command
+        if (inputText === 'power on') {
+            localStorage.setItem('powerStatus', 'on');
+            outputBox.textContent = 'Power is now ON.';
+            document.querySelector('.power').dispatchEvent(new Event('click'));
+        }
+        // Handle 'power off' command
+        else if (inputText === 'power off') {
+            localStorage.setItem('powerStatus', 'off');
+            outputBox.textContent = 'Power is now OFF.';
+        }
+        // Check current power status
+        else if (inputText === 'check power') {
+            var powerStatus = localStorage.getItem('powerStatus') || 'off';
+            outputBox.textContent = 'Power is currently ' + powerStatus.toUpperCase() + '.';
+        }
+        else if (inputText === 'cmd echo') {
+            var prevCommand = localStorage.getItem('prevCommand') || 'no previous command found';
+            outputBox.textContent = prevCommand;
+        }
+        // Handle unrecognized commands
+        else {
+            outputBox.textContent = 'Unrecognized command.';
+        }
+        // Save the command
+        localStorage.setItem('prevCommand', inputText);
+    }
+});
+
